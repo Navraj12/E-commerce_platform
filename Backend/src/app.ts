86b5a890efa11ceabff3680185bfe9aec3ccbaf4
Navraj Awasthi <awasthintr07@gmail.com>
2025-06-27 03:15:23 +0545

    ,,

---
+++ Backend/src/app.ts
@@ -0,0 +1,18 @@
+import { Console } from 'console'
+import express,{Application,Request,Response} from 'express'
+
+const  app:Application = express()
+const  PORT = 3000
+
+
+app.get("/",(req:Request,res:Response) =>{
+res.send("Hello World")
+})
+
+app.get("/about",(req:Request,res:Response) =>{
+res.send("About Page")
+})
+
+app.listen(PORT,()=> {
+console.log("server is running on port",PORT)
+})
