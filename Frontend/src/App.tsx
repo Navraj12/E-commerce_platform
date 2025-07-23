9b894574837064d1f1dee8f5719b83c71645126f
Navraj Awasthi <awasthintr07@gmail.com>
2025-07-23 23:22:59 +0545

    improvement in app.css

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="bg-amber-500 forced-color-adjust-auto">
+      <h1 className="text-3xl font-bold underline">
         Hello guys welcome to my e_commerce{" "}
       </h1>
     </>
