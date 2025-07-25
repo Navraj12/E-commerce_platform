a1454dcf179ba17d38ec1d657937e909fc1e09d5
Navraj Awasthi <awasthintr07@gmail.com>
2025-07-25 14:53:13 +0545

    react and redux connected

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,20 @@
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import store from "./store/store";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello guys welcome to my e_commerce{" "}
-      </h1>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<h1>This is home Page</h1>} />
+            <Route path="/register" element={<h1>This is register Page</h1>} />
+            <Route path="/login" element={<h1>This is login Page</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
