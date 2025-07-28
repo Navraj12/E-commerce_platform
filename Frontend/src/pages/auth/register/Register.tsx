315b8678e90fc9c077b23cbc1812cce1f19b35bc
Navraj Awasthi <awasthintr07@gmail.com>
2025-07-29 00:08:52 +0545

    cors installed in backend

--- Frontend/src/pages/auth/register/Register.tsx
+++ Frontend/src/pages/auth/register/Register.tsx
@@ -1,10 +1,17 @@
 // import { data } from "react-router-dom";
+import axios from "axios";
+import { register } from "../../../store/authSlice";
 import Form from "../Form";
 import type { UserDataType } from "../types";
+import { useDispatch } from "react-redux";
 
 const Register = () => {
-  const handleRegister = (data: UserDataType) => {
+const dispatch = useDispatch()
+
+  const handleRegister = async(data: UserDataType) => {
     console.log(data);
+// dispatch(register(data))
+const response = await axios.post('http://localhost:3000/register',data)
   };
 
   return <Form isRegister={true} onSubmit={handleRegister} />;
