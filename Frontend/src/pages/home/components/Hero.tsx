a8671f9bf175d8e73a35ba0f9470cb94ff08376e
Navraj Awasthi <awasthintr07@gmail.com>
2025-11-15 02:46:36 +0545

    corrected some error from cart

--- Frontend/src/pages/home/components/Hero.tsx
+++ Frontend/src/pages/home/components/Hero.tsx
@@ -7,19 +7,11 @@ const Hero = () => {
       <div className="container relative mx-auto flex flex-col gap-16 px-4 py-16 text-center lg:flex-row lg:gap-0 lg:px-8 lg:py-32 lg:text-left xl:max-w-7xl">
         <div className="lg:flex lg:w-1/2 lg:items-center">
           <div>
-            <div className="mb-2 inline-flex rounded border border-gray-200 bg-gray-100 px-2 py-1 text-sm font-medium leading-4 text-gray-800 dark:border-gray-600/50 dark:bg-gray-700/50 dark:text-gray-200">
-              v6.0 Latest Version
-            </div>
+            <div className="mb-2 inline-flex rounded border border-gray-200 bg-gray-100 px-2 py-1 text-sm font-medium leading-4 text-gray-800 dark:border-gray-600/50 dark:bg-gray-700/50 dark:text-gray-200"></div>
             <h1 className="mb-4 text-4xl font-black text-black dark:text-white">
-              Next generation
-              <span className="text-blue-600 dark:text-blue-500">
-                website builder
-              </span>
+              <span className="text-blue-600 dark:text-blue-500"></span>
             </h1>
-            <h2 className="text-xl font-medium leading-relaxed text-gray-700 dark:text-gray-300">
-              Super fast and easy to use software to power your next idea or
-              build your client’s web projects. Get it today and profit.
-            </h2>
+            <h2 className="text-xl font-medium leading-relaxed text-gray-700 dark:text-gray-300"></h2>
             <div className="flex flex-col justify-center gap-2 pb-16 pt-10 sm:flex-row sm:items-center lg:justify-start">
               <a
                 href="#"
@@ -35,13 +27,13 @@ const Hero = () => {
                   <path d="M10.75 2.75a.75.75 0 00-1.5 0v8.614L6.295 8.235a.75.75 0 10-1.09 1.03l4.25 4.5a.75.75 0 001.09 0l4.25-4.5a.75.75 0 00-1.09-1.03l-2.955 3.129V2.75z" />
                   <path d="M3.5 12.75a.75.75 0 00-1.5 0v2.5A2.75 2.75 0 004.75 18h10.5A2.75 2.75 0 0018 15.25v-2.5a.75.75 0 00-1.5 0v2.5c0 .69-.56 1.25-1.25 1.25H4.75c-.69 0-1.25-.56-1.25-1.25v-2.5z" />
                 </svg>
-                <span>Download for free</span>
+                <span></span>
               </a>
               <a
                 href="#"
                 className="inline-flex items-center justify-center gap-2 rounded-lg border border-gray-200 bg-white px-7 py-3.5 font-semibold leading-6 text-gray-800 hover:border-gray-300 hover:text-gray-900 hover:shadow-sm focus:ring focus:ring-gray-300/25 active:border-gray-200 active:shadow-none dark:border-gray-700 dark:bg-transparent dark:text-gray-300 dark:hover:border-gray-600 dark:hover:text-gray-200 dark:focus:ring-gray-600/40 dark:active:border-gray-700"
               >
-                <span>Learn more</span>
+                <span></span>
               </a>
             </div>
           </div>
@@ -55,14 +47,13 @@ const Hero = () => {
             <div className="absolute inset-0 -m-6 -rotate-2 rounded-xl bg-gray-200 dark:bg-gray-800" />
             <div className="absolute inset-0 -m-6 rotate-1 rounded-xl bg-blue-800/75 shadow-inner dark:bg-blue-900/75" />
             <img
-              src="https://cdn.tailkit.com/media/placeholders/photo-RSCirJ70NDM-800x1000.jpg"
+              src=""
               className="relative mx-auto rounded-lg shadow-lg"
-              alt="Hero Image"
+              alt=""
             />
           </div>
         </div>
       </div>
-      {/* END Hero Content */}
     </div>
   );
 };
